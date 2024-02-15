ab6de96d0e3bf4e0cff9582b3413f48d8c75e098
Jasmine Allen <jaguilar@stu.jsu.edu>
2024-02-15 13:39:28 -0600

    Fixed button issues and constraints. Fixed string-array orders for btnLabels and btnTags.

--- app/src/main/java/edu/jsu/mcis/cs408/calculator/MainActivity.java
+++ app/src/main/java/edu/jsu/mcis/cs408/calculator/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.view.ViewGroup.LayoutParams;
 import edu.jsu.mcis.cs408.calculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,28 +26,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initLayout() {
+
         ConstraintLayout layout = binding.layout;
-        ConstraintSet set = new ConstraintSet();
-
-        TextView tv = createTextView();
-        layout.addView(tv);
-
-        set.clone(layout);
-        set.connect(tv.getId(), ConstraintSet.START, binding.guideWest.getId(), ConstraintSet.END, 0);
-        set.connect(tv.getId(), ConstraintSet.END, binding.guideEast.getId(), ConstraintSet.START, 0);
-        set.connect(tv.getId(), ConstraintSet.TOP, binding.guideNorth.getId(), ConstraintSet.BOTTOM, 0);
-        set.constrainWidth(tv.getId(), ConstraintSet.MATCH_CONSTRAINT);
-        set.constrainHeight(tv.getId(), ConstraintSet.WRAP_CONTENT);
-        set.applyTo(layout);
 
         int[][] horizontals = new int[KEYS_HEIGHT][KEYS_WIDTH];
         int[][] verticals = new int[KEYS_WIDTH][KEYS_HEIGHT];
 
+        String[] btnLabelsArray = getResources().getStringArray(R.array.btnLabels);
+        String[] btnTagsArray = getResources().getStringArray(R.array.btnTags);
+
+        TextView tv = createTextView();
+        layout.addView(tv);
+
         for (int row = 0; row < KEYS_HEIGHT; ++row) {
+
             for (int col = 0; col < KEYS_WIDTH; ++col) {
+
                 int id = View.generateViewId();
-                String[] btnLabelsArray = getResources().getStringArray(R.array.btnLabels);
-                String[] btnTagsArray = getResources().getStringArray(R.array.btnTags);
 
                 Button key = new Button(this);
                 key.setId(id);
@@ -55,13 +51,29 @@ public class MainActivity extends AppCompatActivity {
                 key.setTextSize(24);
                 layout.addView(key);
 
+                LayoutParams params = key.getLayoutParams();
+                params.width = ConstraintLayout.LayoutParams.MATCH_CONSTRAINT;
+                params.height = ConstraintLayout.LayoutParams.MATCH_CONSTRAINT;
+                key.setLayoutParams(params);
+
                 horizontals[row][col] = id;
                 verticals[col][row] = id;
 
-                set.connect(key.getId(), ConstraintSet.TOP, tv.getId(), ConstraintSet.BOTTOM, 16); // Adjust the spacing
-                set.connect(key.getId(), ConstraintSet.START, binding.guideWest.getId(), ConstraintSet.END, col * 8); // Adjust the spacing
+
             }
         }
+
+        ConstraintSet set = new ConstraintSet();
+        set.clone(layout);
+
+        set.connect(tv.getId(), ConstraintSet.START, binding.guideWest.getId(), ConstraintSet.END, 0);
+        set.connect(tv.getId(), ConstraintSet.END, binding.guideEast.getId(), ConstraintSet.START, 0);
+        set.connect(tv.getId(), ConstraintSet.TOP, binding.guideNorth.getId(), ConstraintSet.BOTTOM, 0);
+        set.constrainWidth(tv.getId(), ConstraintSet.MATCH_CONSTRAINT);
+        set.constrainHeight(tv.getId(), ConstraintSet.WRAP_CONTENT);
+
+        //set.connect(key.getId(), ConstraintSet.TOP, tv.getId(), ConstraintSet.BOTTOM, 16); // Adjust the spacing
+        //set.connect(key.getId(), ConstraintSet.START, binding.guideWest.getId(), ConstraintSet.END, col * 8); // Adjust the spacing
 
         for (int row = 0; row < KEYS_HEIGHT; ++row) {
             set.createHorizontalChain(binding.guideWest.getId(), ConstraintSet.LEFT, binding.guideEast.getId(), ConstraintSet.RIGHT, horizontals[row], null, ConstraintSet.CHAIN_PACKED);
@@ -70,8 +82,10 @@ public class MainActivity extends AppCompatActivity {
         for (int col = 0; col < KEYS_WIDTH; ++col) {
             set.createVerticalChain(tv.getId(), ConstraintSet.BOTTOM, binding.guideSouth.getId(), ConstraintSet.BOTTOM, verticals[col], null, ConstraintSet.CHAIN_PACKED);
         }
-    }
 
+        set.applyTo(layout);
+
+    }
 
     private TextView createTextView() {
         int id = View.generateViewId();
